39568b7eaf69500007f5ccd55ff35cc8a42ffe13
Rob Sturcke <rob@rslabs.dev>
2024-01-23 23:41:42 -0500

    Initial Commit

---
+++ DockProgress/DockProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockProgressApp.swift
+//  DockProgress
+//
+//  Created by Rob Sturcke on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct DockProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
